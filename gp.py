16adcb44d79d72d559f5fa277ae3eb4b1fe60255
Nathan Hartland <n.p.hartland@vu.nl>
2018-04-03 19:50:26 +0200

    Move to command line arguments for specification of prior and number of GP samples

--- gp.py
+++ gp.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 """
     Gaussian Process Parton Distribution Functions (GPPDFs)
 
@@ -15,68 +15,79 @@
         - The grid of x-points sampled in the GP
         - The GP mean function evaluated on the x-grid
         - The GP covariance function evaluated on the x-grid
-        - A numpy array of `ngen` samples of the GP
+        - A numpy array of `nsamples` samples of the GP
 
-    The sample array has shape (`ngen`, `nx*nf`) where `nx` is the number of
+    The sample array has shape (`nsamples`, `nx*nf`) where `nx` is the number of
     points in the sampled x-grid, and `nf` is the number of active flavours in
     the GP. The x-grid points are currently hardcoded in xg.py, the flavour basis
     is read from the prior LHAPDF set.
 """
 
 import lhapdf
-import numpy as np
+import argparse
 import itertools
+import numpy as np
 from xg import XGRID
 
-# Number of gaussian processes
-ngen_gp = 100000
 
-# Prior PDF
-# prior = "180307-nh-002"
-prior  = "171113-nh-002"
-pdfset = lhapdf.getPDFSet(prior)
-replicas  = pdfset.mkPDFs()[1:]
+def generate_gp(prior, nsamples):
+    """ Generate the GP and `nsamples` GP samples from a `prior` PDF """
 
-# Available flavours
-flavour_string = pdfset.get_entry("Flavors").split(",")
-nfl, flavours = len(flavour_string), list(map(int, flavour_string))
+    pdfset = lhapdf.getPDFSet(prior)
+    replicas  = pdfset.mkPDFs()[1:]
 
-# Initial scale and x-grid
-Q0 = float(pdfset.get_entry("QMin"))
-xs, nx = XGRID, len(XGRID)
+    # Available flavours
+    flavour_string = pdfset.get_entry("Flavors").split(",")
+    nfl, flavours = len(flavour_string), list(map(int, flavour_string))
 
-print(f"Sampling {nx} x-points at initial scale: {Q0} GeV")
-grid_points = list(itertools.product(flavours, xs))
-pdf_values = np.empty([nfl*nx, len(replicas)])
-for irep, rep in enumerate(replicas):
-    for ipt, pt in enumerate(grid_points):
-        pdf_values[ipt][irep] = rep.xfxQ(pt[0], pt[1], Q0)
+    # Initial scale and x-grid
+    Q0 = float(pdfset.get_entry("QMin"))
+    xs, nx = XGRID, len(XGRID)
 
-print("Computing stats")
-mean       = np.mean(pdf_values, axis=1)
-covariance = np.cov(pdf_values)
+    print(f"Sampling {nx} x-points at initial scale: {Q0} GeV")
+    grid_points = list(itertools.product(flavours, xs))
+    pdf_values = np.empty([nfl*nx, len(replicas)])
+    for irep, rep in enumerate(replicas):
+        for ipt, pt in enumerate(grid_points):
+            pdf_values[ipt][irep] = rep.xfxQ(pt[0], pt[1], Q0)
 
-# Condition covariance matrix a bit
-# https://stackoverflow.com/questions/41515522/numpy-positive-semi-definite-warning
-# This is due to including b in the covariance matrix
-min_eig = np.min(np.real(np.linalg.eigvals(covariance)))
-while min_eig < 0:
-    print("WARNING: Covariance matrix not positive-semidefinite")
-    print(f"Minimum eigenvalue: {min_eig}")
-    print("Introducing regulator...")
-    covariance -= 100*min_eig * np.eye(*covariance.shape)
+    print("Computing stats")
+    mean       = np.mean(pdf_values, axis=1)
+    covariance = np.cov(pdf_values)
+
+    # Condition covariance matrix a bit
+    # https://stackoverflow.com/questions/41515522/numpy-positive-semi-definite-warning
+    # This is due to including b in the covariance matrix
     min_eig = np.min(np.real(np.linalg.eigvals(covariance)))
-    print(f"New minimum: {min_eig}")
+    while min_eig < 0:
+        print("WARNING: Covariance matrix not positive-semidefinite")
+        print(f"Minimum eigenvalue: {min_eig}")
+        print("Introducing regulator...")
+        covariance -= 100*min_eig * np.eye(*covariance.shape)
+        min_eig = np.min(np.real(np.linalg.eigvals(covariance)))
+        print(f"New minimum: {min_eig}")
 
-# Generate gaussian processes
-print("Generating GPs")
-gp_values = np.random.multivariate_normal(mean, covariance, ngen_gp, 'raise')
+    # Generate gaussian processes
+    print("Generating GPs")
+    gp_values = np.random.multivariate_normal(mean, covariance, nsamples, 'raise')
 
-np.savez_compressed(f'GP_{prior}_{len(gp_values)}',
-                    prior=prior,
-                    setname=f'GP_{prior}_{len(gp_values)}',
-                    mean=mean,
-                    covariance=covariance, Q0=Q0,
-                    flavours=flavours, xgrid=xs,
-                    samples=gp_values)
+    outfile = f'GP_{prior}_{len(gp_values)}'
+    np.savez_compressed(outfile,
+                        prior=prior,
+                        setname=f'GP_{prior}_{len(gp_values)}',
+                        mean=mean,
+                        covariance=covariance, Q0=Q0,
+                        flavours=flavours, xgrid=xs,
+                        samples=gp_values)
+    return outfile
+
+
+#TODO x-grid selection, separate out GP definition from sample generation
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("priorpdf", help="prior LHAPDF grid used to generate GP", type=str)
+    parser.add_argument("nsamples", help="number of GP samples", type=int)
+    args = parser.parse_args()
+    outfile = generate_gp(args.priorpdf, args.nsamples)
+    print(f'Results output to {outfile}')
 
